d85e810e54c306b0ce774b16a529a24580f5eb82
Delvmware <bauwens.Delphine@gmail.com>
2023-04-13 09:16:28 +0200

    init projet

---
+++ src/main/java/fr/studi/insterspec/InsterspecApplication.java
@@ -0,0 +1,13 @@
+package fr.studi.insterspec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsterspecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsterspecApplication.class, args);
+	}
+
+}
