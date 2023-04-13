7f67f50cfe9082b0311d7b40a85b9ec894ecccfb
Delvmware <bauwens.Delphine@gmail.com>
2023-04-13 09:16:28 +0200

    implement java entity

---
+++ src/test/java/fr/studi/insterspec/InsterspecApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.studi.insterspec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsterspecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
